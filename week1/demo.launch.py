80d5e82fb835da649cbf86355c9bbb2de6c86878
LEE DO YOON <ehdbs200147@gmail.com>
2026-07-24 21:20:07 +0900

    refactor: organize week1 practice files and logs, add .gitignore

---
+++ week1/demo.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+	return LaunchDescription([
+		Node(package="turtlesim", executable="turtlesim_node"),
+		Node(package="turtlesim", executable="turtle_teleop_key",
+		prefix="xterm -e"),
+	]) 
